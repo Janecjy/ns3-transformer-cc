f7eba511aa7a3fb4c9f8c47b824357fd49216bb1
Jane Chen <janechen@cs.utexas.edu>
2024-04-25 05:03:22 -0400

    Remove some on-off patterns

--- tacc-scripts/genJob.py
+++ tacc-scripts/genJob.py
@@ -6,8 +6,7 @@ import random
 
 trace_dir = sys.argv[1]
 output_dir = sys.argv[2]
-
-types = ["TcpNewReno", "TcpCubic"]
+type = sys.argv[3]
 
 # for all traces in the directory, add to list
 traces = []
@@ -15,24 +14,23 @@ for file in os.listdir(trace_dir):
     if file.endswith(".log"):
         traces.append(trace_dir+file)
         
-on_mean = ["0.1", "0.5", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-on_var = ["0.01", "0.05", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"]
-off_mean = ["0.02", "0.1", "0.2", "0.4", "0.5", "0.6", "0.8", "1", "1.2", "1.4", "1.6", "1.8", "2"]
-off_var = ["0.005", "0.01", "0.05", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"]
+on_mean = ["0.1", "0.5", "1", "2", "5", "7", "10"]
+on_var = ["0.01", "0.05", "0.1", "0.2", "0.5", "0.8", "1"]
+off_mean = ["0.02", "0.1", "0.2", "0.5", "0.8", "1", "1.5", "2"]
+off_var = ["0.005", "0.01", "0.05", "0.1", "0.5", "1"]
 
-for om in on_mean:
-    for ov in on_var:
-        for ofm in off_mean:
-            for ofv in off_var:
-                    for t in types:
-                        for trace in traces:
-                            # get line number of the trace file
-                            with open(trace) as f:
-                                for i, l in enumerate(f):
-                                    pass
-                                trace_line_num = i + 1
-                            for i in range(0, int(trace_line_num/10)):
-                                #get a random number between 0 and line_num - 10
-                                start_line = random.randint(0, trace_line_num-10)
-                                cmd = './ns3 run "scratch/scratch-simulator --tcpTypeId='+t+' --stopTime=10 --traceFile='+trace+' --outputDir='+output_dir+' --onTimeMean='+om+' --onTimeVar='+ov+' --offTimeMean='+ofm+' --offTimeVar='+ofv+' --startLine='+str(start_line)+'"'
-                                print(cmd)
+for test in range(10):
+    for om in on_mean:
+        for ov in on_var:
+            for ofm in off_mean:
+                for ofv in off_var:
+                    for trace in traces:
+                        # get line number of the trace file
+                        with open(trace) as f:
+                            for i, l in enumerate(f):
+                                pass
+                            trace_line_num = i + 1
+                        for i in range(0, int(trace_line_num/10)):
+                            #get a random number between 0 and line_num - 10
+                            start_line = random.randint(0, trace_line_num-10)
+                            cmd = './ns3 run "scratch/scratch-simulator --tcpTypeId='+type+' --stopTime=10 --traceFile='+trace+' --outputDir='+output_dir+' --onTimeMean='+om+' --onTimeVar='+ov+' --offTimeMean='+ofm+' --offTimeVar='+ofv+' --startLine='+str(start_line)+'"'
