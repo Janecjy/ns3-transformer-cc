67e92e9918e60c73087c0306a2ebe5260b077dca
Jane Chen <janechen@cs.utexas.edu>
2024-06-17 14:04:09 -0500

    move exp_per_file iteration in the subscript

--- tacc-scripts/genSwitchJob.py
+++ tacc-scripts/genSwitchJob.py
@@ -4,17 +4,16 @@ import random
 
 parent_dir = "/scratch/09498/janechen/"
 trace_dir = "/scratch/09498/janechen/ns3-traces/"
-exp_per_file = 20
+exp_per_file = 5
 
 def main():
     print("max_jobs=48; cur_jobs=0")
     for root, dirs, files in os.walk(trace_dir):
         if files:
             for file in files:
-                for i in range(exp_per_file):
-                    print("((cur_jobs >= max_jobs)) && wait -n")
-                    cmd = "python /home1/09498/janechen/ns3-transformer-cc/tacc-scripts/genSwitchJobSub.py " + root + " " + file + " " + "switch_output_avg_" + str(exp_per_file)
-                    print(cmd+" & ((++cur_jobs))")
+                print("((cur_jobs >= max_jobs)) && wait -n")
+                cmd = "python /home1/09498/janechen/ns3-transformer-cc/tacc-scripts/genSwitchJobSub.py " + root + " " + file + " " + "switch_output_avg_" + str(exp_per_file)
+                print(cmd+" & ((++cur_jobs))")
     print("wait")
 
 if __name__ == "__main__":
