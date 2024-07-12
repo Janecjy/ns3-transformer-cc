e48c9a76d151e91ef150285f263b6271c5b3b4d4
Jane Chen <janechen@cs.utexas.edu>
2024-07-12 02:55:33 -0500

    Change scaled traces directory path

--- tacc-scripts/genTransformerTrainData.sh
+++ tacc-scripts/genTransformerTrainData.sh
@@ -1,63 +1,51 @@
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.7-0.4/bus/ bus TcpCubic --beta=0.7 --cubicC=0.4 > cubic-0.7-0.4-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.7-0.4/bus/ bus TcpCubic --beta=0.7 --cubicC=0.4 > cubic-5x-0.7-0.4-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.7-0.4/bus/ bus TcpCubic --beta=0.7 --cubicC=0.4 > cubic-25x-0.7-0.4-bus.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.7-0.4/bus/ bus TcpCubic --beta=0.7 --cubicC=0.4 > cubic-10x-0.7-0.4-bus.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.7-0.4/bus/ bus TcpCubic --beta=0.7 --cubicC=0.4 > cubic-25x-0.7-0.4-bus.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.5-0.4/car/ car TcpCubic --beta=0.5 --cubicC=0.4 > cubic-0.5-0.4-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.5-0.4/car/ car TcpCubic --beta=0.5 --cubicC=0.4 > cubic-5x-0.5-0.4-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.5-0.4/car/ car TcpCubic --beta=0.5 --cubicC=0.4 > cubic-25x-0.5-0.4-car.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.5-0.4/car/ car TcpCubic --beta=0.5 --cubicC=0.4 > cubic-10x-0.5-0.4-car.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.5-0.4/car/ car TcpCubic --beta=0.5 --cubicC=0.4 > cubic-25x-0.5-0.4-car.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.5-0.8/ferry/ ferry TcpCubic --beta=0.5 --cubicC=0.8 > cubic-0.5-0.8-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.5-0.8/ferry/ ferry TcpCubic --beta=0.5 --cubicC=0.8 > cubic-5x-0.5-0.8-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.5-0.8/ferry/ ferry TcpCubic --beta=0.5 --cubicC=0.8 > cubic-25x-0.5-0.8-ferry.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.5-0.8/ferry/ ferry TcpCubic --beta=0.5 --cubicC=0.8 > cubic-10x-0.5-0.8-ferry.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.5-0.8/ferry/ ferry TcpCubic --beta=0.5 --cubicC=0.8 > cubic-25x-0.5-0.8-ferry.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.7-0.8/metro/ metro TcpCubic --beta=0.7 --cubicC=0.8 > cubic-0.7-0.8-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.7-0.8/metro/ metro TcpCubic --beta=0.7 --cubicC=0.8 > cubic-5x-0.7-0.8-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.7-0.8/metro/ metro TcpCubic --beta=0.7 --cubicC=0.8 > cubic-25x-0.7-0.8-metro.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.7-0.8/metro/ metro TcpCubic --beta=0.7 --cubicC=0.8 > cubic-10x-0.7-0.8-metro.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.7-0.8/metro/ metro TcpCubic --beta=0.7 --cubicC=0.8 > cubic-25x-0.7-0.8-metro.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.8-0.4/train/ train TcpCubic --beta=0.8 --cubicC=0.4 > cubic-0.8-0.4-train.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.8-0.4/train/ train TcpCubic --beta=0.8 --cubicC=0.4 > cubic-5x-0.8-0.4-train.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.8-0.4/train/ train TcpCubic --beta=0.8 --cubicC=0.4 > cubic-25x-0.8-0.4-train.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.8-0.4/train/ train TcpCubic --beta=0.8 --cubicC=0.4 > cubic-10x-0.8-0.4-train.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.8-0.4/train/ train TcpCubic --beta=0.8 --cubicC=0.4 > cubic-25x-0.8-0.4-train.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.8-0.8/tram/ tram TcpCubic --beta=0.8 --cubicC=0.8 > cubic-0.8-0.8-tram.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.8-0.8/tram/ tram TcpCubic --beta=0.8 --cubicC=0.8 > cubic-5x-0.8-0.8-tram.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.8-0.8/tram/ tram TcpCubic --beta=0.8 --cubicC=0.8 > cubic-25x-0.8-0.8-tram.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.8-0.8/tram/ tram TcpCubic --beta=0.8 --cubicC=0.8 > cubic-10x-0.8-0.8-tram.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.8-0.8/tram/ tram TcpCubic --beta=0.8 --cubicC=0.8 > cubic-25x-0.8-0.8-tram.sh
 
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.9-0.4/bus/ bus TcpCubic --beta=0.9 --cubicC=0.4 > cubic-0.9-0.4-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.9-0.4/bus/ bus TcpCubic --beta=0.9 --cubicC=0.4 > cubic-5x-0.9-0.4-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.9-0.4/bus/ bus TcpCubic --beta=0.9 --cubicC=0.4 > cubic-25x-0.9-0.4-bus.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.9-0.4/bus/ bus TcpCubic --beta=0.9 --cubicC=0.4 > cubic-10x-0.9-0.4-bus.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.9-0.4/bus/ bus TcpCubic --beta=0.9 --cubicC=0.4 > cubic-25x-0.9-0.4-bus.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic/Cubic-0.9-0.8/car/ car TcpCubic --beta=0.9 --cubicC=0.8 > cubic-0.9-0.8-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-5x/Cubic-0.9-0.8/car/ car TcpCubic --beta=0.9 --cubicC=0.8 > cubic-5x-0.9-0.8-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/Cubic-25x/Cubic-0.9-0.8/car/ car TcpCubic --beta=0.9 --cubicC=0.8 > cubic-25x-0.9-0.8-car.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/Cubic-10x/Cubic-0.9-0.8/car/ car TcpCubic --beta=0.9 --cubicC=0.8 > cubic-10x-0.9-0.8-car.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/Cubic-25x/Cubic-0.9-0.8/car/ car TcpCubic --beta=0.9 --cubicC=0.8 > cubic-25x-0.9-0.8-car.sh
 
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-1-2/ferry/ ferry TcpNewReno --alpha=1 --renoBeta=2 > reno-1-2-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-1-2/ferry/ ferry TcpNewReno --alpha=1 --renoBeta=2 > reno-5x-1-2-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-1-2/ferry/ ferry TcpNewReno --alpha=1 --renoBeta=2 > reno-25x-1-2-ferry.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-1-2/ferry/ ferry TcpNewReno --alpha=1 --renoBeta=2 > reno-10x-1-2-ferry.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-1-2/ferry/ ferry TcpNewReno --alpha=1 --renoBeta=2 > reno-25x-1-2-ferry.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-1-4/metro/ metro TcpNewReno --alpha=1 --renoBeta=4 > reno-1-4-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-1-4/metro/ metro TcpNewReno --alpha=1 --renoBeta=4 > reno-5x-1-4-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-1-4/metro/ metro TcpNewReno --alpha=1 --renoBeta=4 > reno-25x-1-4-metro.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-1-4/metro/ metro TcpNewReno --alpha=1 --renoBeta=4 > reno-10x-1-4-metro.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-1-4/metro/ metro TcpNewReno --alpha=1 --renoBeta=4 > reno-25x-1-4-metro.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-1.5-2/train/ train TcpNewReno --alpha=1.5 --renoBeta=2 > reno-1.5-2-train.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-1.5-2/train/ train TcpNewReno --alpha=1.5 --renoBeta=2 > reno-5x-1.5-2-train.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-1.5-2/train/ train TcpNewReno --alpha=1.5 --renoBeta=2 > reno-25x-1.5-2-train.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-1.5-2/train/ train TcpNewReno --alpha=1.5 --renoBeta=2 > reno-10x-1.5-2-train.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-1.5-2/train/ train TcpNewReno --alpha=1.5 --renoBeta=2 > reno-25x-1.5-2-train.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-1.5-4/tram/ tram TcpNewReno --alpha=1.5 --renoBeta=4 > reno-1.5-4-tram.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-1.5-4/tram/ tram TcpNewReno --alpha=1.5 --renoBeta=4 > reno-5x-1.5-4-tram.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-1.5-4/tram/ tram TcpNewReno --alpha=1.5 --renoBeta=4 > reno-25x-1.5-4-tram.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-1.5-4/tram/ tram TcpNewReno --alpha=1.5 --renoBeta=4 > reno-10x-1.5-4-tram.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-1.5-4/tram/ tram TcpNewReno --alpha=1.5 --renoBeta=4 > reno-25x-1.5-4-tram.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-2-2/bus/ bus TcpNewReno --alpha=2 --renoBeta=2 > reno-2-2-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-2-2/bus/ bus TcpNewReno --alpha=2 --renoBeta=2 > reno-5x-2-2-bus.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-2-2/bus/ bus TcpNewReno --alpha=2 --renoBeta=2 > reno-25x-2-2-bus.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-2-2/bus/ bus TcpNewReno --alpha=2 --renoBeta=2 > reno-10x-2-2-bus.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-2-2/bus/ bus TcpNewReno --alpha=2 --renoBeta=2 > reno-25x-2-2-bus.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-2-4/car/ car TcpNewReno --alpha=2 --renoBeta=4 > reno-2-4-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-2-4/car/ car TcpNewReno --alpha=2 --renoBeta=4 > reno-5x-2-4-car.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-2-4/car/ car TcpNewReno --alpha=2 --renoBeta=4 > reno-25x-2-4-car.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-2-4/car/ car TcpNewReno --alpha=2 --renoBeta=4 > reno-10x-2-4-car.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-2-4/car/ car TcpNewReno --alpha=2 --renoBeta=4 > reno-25x-2-4-car.sh
 
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-5-1.5/ferry/ ferry TcpNewReno --alpha=5 --renoBeta=1.5 > reno-5-1.5-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-5-1.5/ferry/ ferry TcpNewReno --alpha=5 --renoBeta=1.5 > reno-5x-5-1.5-ferry.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-5-1.5/ferry/ ferry TcpNewReno --alpha=5 --renoBeta=1.5 > reno-25x-5-1.5-ferry.sh
-
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-5-1.5/ferry/ ferry TcpNewReno --alpha=5 --renoBeta=1.5 > reno-10x-5-1.5-ferry.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-5-1.5/ferry/ ferry TcpNewReno --alpha=5 --renoBeta=1.5 > reno-25x-5-1.5-ferry.sh
 python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno/NewReno-5-3/metro/ metro TcpNewReno --alpha=5 --renoBeta=3 > reno-5-3-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-5x/NewReno-5-3/metro/ metro TcpNewReno --alpha=5 --renoBeta=3 > reno-5x-5-3-metro.sh
-python genJob.py /scratch/09498/janechen/ns3-traces/ /scratch/09498/janechen/NewReno-25x/NewReno-5-3/metro/ metro TcpNewReno --alpha=5 --renoBeta=3 > reno-25x-5-3-metro.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-10.0x/ /scratch/09498/janechen/NewReno-10x/NewReno-5-3/metro/ metro TcpNewReno --alpha=5 --renoBeta=3 > reno-10x-5-3-metro.sh
+python genJob.py /scratch/09498/janechen/ns3-traces-25.0x/ /scratch/09498/janechen/NewReno-25x/NewReno-5-3/metro/ metro TcpNewReno --alpha=5 --renoBeta=3 > reno-25x-5-3-metro.sh
